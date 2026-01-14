8a80895e1f1b925db87be4cd58ef0e4a5802d5f2
Dávid Mezei <mezei.david.sandor@diak.szbi-pg.hu>
2026-01-14 15:49:06 +0100

    Create main menu, gamemode file

---
+++ gamemodes.py
@@ -0,0 +1,8 @@
+def gamemode_easy():
+    print("EASY")
+
+def gamemode_hard():
+    print("HARD")
+
+def gamemode_hardcore():
+    print("HARDCORE")
---
+++ main_menu.py
@@ -0,0 +1,40 @@
+from rich import print as rprint
+from gamemodes import *
+def main_menu():
+    print("""
+          
+
+ .d8888b.  d8b 888              .d8888b.                                             
+d88P  Y88b Y8P 888             d88P  Y88b                                            
+888    888     888             888    888                                            
+888        888 888888 888  888 888        888  888  .d88b.  .d8888b  .d8888b  888d888
+888        888 888    888  888 888  88888 888  888 d8P  Y8b 88K      88K      888P"  
+888    888 888 888    888  888 888    888 888  888 88888888 "Y8888b. "Y8888b. 888    
+Y88b  d88P 888 Y88b.  Y88b 888 Y88b  d88P Y88b 888 Y8b.          X88      X88 888    
+ "Y8888P"  888  "Y888  "Y88888  "Y8888P88  "Y88888  "Y8888   88888P'  88888P' 888    
+                           888                                                       
+                      Y8b d88P                                                       
+                       "Y88P"                                                        
+
+""")
+    
+
+    print("Welcome to our game!")
+    print("You can choose from 3 difficulties:")
+    rprint("[#00C8FF] - 1. Easy (Only Countries) [/#00C8FF]")
+    rprint("[#00FF51] - 2. Hard (Countries and capitals; >10 characters) [/#00FF51]")
+    rprint("[#E30E0E] - 3. Hardcore (Countries and capitals; 10+ characters) [/#E30E0E]")
+
+    print("-----------------------------------")
+    try:
+        jatekmod = int(input("Which one do you want to try? (1, 2, 3) "))
+        if jatekmod == 1:
+            gamemode_easy()
+        elif jatekmod == 2:
+            gamemode_hard()
+        else:
+            gamemode_hardcore()
+    except ValueError:
+        print("Please enter a correct number! (1-3)")
+
+main_menu()
